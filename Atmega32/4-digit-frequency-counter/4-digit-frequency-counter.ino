2c089a522c687686f4f8a57e8c29e7c79278214e
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2024-02-23 03:57:21 +0600

    Update 4-digit-frequency-counter.ino

--- Atmega32/4-digit-frequency-counter/4-digit-frequency-counter.ino
+++ Atmega32/4-digit-frequency-counter/4-digit-frequency-counter.ino
@@ -1,4 +1,4 @@
- #include <LiquidCrystal.h>
+#include <LiquidCrystal.h>
 
 const int pulsePin = 7;
 
@@ -8,69 +8,102 @@ float pulseTotal;
 float frequency; 
   
   
-  const int digitPins[] = {2, 3, 4, 5}; 
-  const int segmentPins[] = {8, 9, 10, 11}; 
-  
-  const byte digitSegments[10] = {
-    0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0X09};
+  const int segmentPins[] = {2, 3, 4, 5};//seg 1,2,3,4(from left)
+  const int digitPins[] = {8, 9, 10, 11};//digit pin(1,2,4,8)
 
-    void setup() {
-      Serial.begin(9600);
-          pinMode(pulsePin,INPUT);
+  const byte digitSegments[12] = {
+    0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0X09,0x0C,0x0A};
 
+ void setup() {
+      pinMode(pulsePin, INPUT);
       for (int i = 0; i < 4; i++) {
-        pinMode(digitPins[i], OUTPUT);
         pinMode(segmentPins[i], OUTPUT);
+        pinMode(digitPins[i], OUTPUT);
+        digitalWrite(segmentPins[i], LOW);
         digitalWrite(digitPins[i], LOW);
+      }
+}
+
+ void displayDigit(int segment, int value) {
+      for (int i = 0; i < 4; i++) {
         digitalWrite(segmentPins[i], LOW);
       }
-    }
-
- void displayDigit(int digit, int value) {
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(digitPins[i], LOW);
+      for (int i = 0; i < 10; i++) {
+        digitalWrite(digitPins[i], bitRead(digitSegments[value], i));
       }
-      for (int i = 0; i < 7; i++) {
-        digitalWrite(segmentPins[i], bitRead(digitSegments[value], i));
-      }
-      digitalWrite(digitPins[digit-1],HIGH);
+      digitalWrite(segmentPins[segment-1],HIGH);
       delay(5);
     }
 
-    
-void clearDisplay(){
-  for (int i = 0; i < 4; i++) 
-        digitalWrite(digitPins[i], LOW);
-   for (int i = 0; i < 4; i++) 
-        digitalWrite(segmentPins[i], LOW); 
+ void displayNumber(String val)
+    { 
+      int number;
+      String ss;
       
-}
-
-
- void displayNumber(int number)
-    {
-      if(number == 0)
+      for(int i=0;i<val.length();i++)
       {
-        PORTB = 0x00;
-         for (int i = 0; i < 4; i++){ 
-           digitalWrite(digitPins[i], HIGH);}
+        if(isdigit(val[i]))
+           ss+=val[i];
       }
+      if(ss.length()<=4)
+      {
+        if(ss=="0000")
+        for(int i=0;i<4;i++)
+          displayDigit(i+1,0);
+      
+        if(ss=="000")
+          for(int i=0;i<3;i++)
+              displayDigit(i+1,0);
+              
+        if(ss=="00")
+          for(int i=0;i<2;i++)
+              displayDigit(i+1,0);
+              
+        if(ss=="0"&& ss[0]!='0')
+              displayDigit(1,0);
+              
+        if(ss.length()==1 &&ss[0]=='0')
+          displayDigit(ss.length(),0);
+          
+        if(ss.length()<=4 &&ss[0]=='0' && ss[1]!='0')
+          displayDigit(ss.length(),0);
+          
+        if(ss.length()<=4 && ss[0]=='0' && ss[1]=='0' && ss[2]!='0' )
+        {
+          displayDigit(ss.length(),0);
+          displayDigit(ss.length()-1,0);
+        }
+        if(ss.length()==4 &&ss[0]=='0' && ss[1]=='0' && ss[2]=='0' && ss[3]!='0' )
+        {
+          displayDigit(ss.length(),0);
+          displayDigit(ss.length()-1,0);
+          displayDigit(ss.length()-2,0);
+        }
+      
+      number=ss.toInt();
       
       int rem =0;
-      int count = 0;  
+      int count = 0;
         while (number > 0) {
           rem = number%10;
-          number = number / 10;     
+          number = number / 10;
           displayDigit(count+1,rem);
-          count++;  
+          count++;
          }
-         
-    }
 
+      }
+      else
+      {
+        displayDigit(1,10);
+        displayDigit(2,11);
+        displayDigit(3,10);
+        displayDigit(4,11);
+      }    
+ }
 
 void loop() {
 
-   pulseHigh = pulseIn(pulsePin,HIGH);
+    pulseHigh = pulseIn(pulsePin,HIGH);
     pulseLow = pulseIn(pulsePin,LOW);
     pulseTotal = pulseHigh + pulseLow;
     
@@ -80,5 +113,5 @@ void loop() {
      Serial.println(frequency);
     
   
-        displayNumber(frequency);
+     displayNumber(String(frequency));
 }
